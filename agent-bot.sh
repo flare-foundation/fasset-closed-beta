f90c933b7e0a960b19c1ad262dddd19108c2c08b
Iztok <iztok@abelium.com>
2024-05-29 16:14:01 +0200

    init, run, and cli scripts

---
+++ agent-bot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+node ../fasset-bots/packages/fasset-bots-cli/dist/src/cli/agent-bot.js "$@"
--- init.sh
+++ init.sh
@@ -1,2 +1,13 @@
-check if ../fasset-bots exists
-copy files over
+#!/bin/bash
+
+set -e
+
+if [ ! -d ../fasset-bots ]; then
+    echo "Missing directory ../fasset-bots. Please cehckout 'https://github.com/flare-labs-ltd/fasset-bots.git' in the parent directory."
+    exit 1
+fi
+
+cd ../fasset-bots
+yarn
+yarn clean
+yarn build
