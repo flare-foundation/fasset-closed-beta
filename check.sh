e259e66065efea25a82d1e468b99ae3179c0a55e
kuco23 <nseverkar@gmail.com>
2024-05-30 16:17:43 +0200

    better check

--- check.sh
+++ check.sh
@@ -1,1 +1,8 @@
-echo $(./user-bot.sh agents --all -f FfakeXRP | grep -oP ^[0-9a-zA-Z]{40})
+address=0xd2150783D0be9D8ce64B956dbF98c09FdBccB0
+agents=$(./user-bot.sh agents --all -f FfakeXRP | grep -oP ^[0-9a-zA-Z]{40})
+
+if echo "$agents" | grep -q "$address"; then
+  echo "Correct configuration setup!!"
+else
+  echo "Invalid configuration setup!"
+fi
