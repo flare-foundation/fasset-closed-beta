c76d9527036c7c4c70005e94e0b316f2dc1b498e
david.pangerl <david.pangerl@flare.network>
2024-06-07 13:50:22 +0200

    test

--- check.sh
+++ check.sh
@@ -1,8 +1,17 @@
-address=0xd2150783D0be9D8ce64B956dbF98c09FdBccB0
-agents=$(./user-bot.sh agents --all -f FfakeXRP | grep -Eo ^[0-9a-zA-Z]{40})
+address=0x295B53615eE31a92a83514cE10AB78487d04D258
+agents=$(./user-bot.sh agents --all -f FSimCoinX | grep -Eo ^[0-9a-zA-Z]{42})
+
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export WHITE="\033[0;37m"
+export NC='\033[0m' # No Color
+export REDBOLD="${RED}$(tput bold)"
+export GREENBOLD="${GREEN}$(tput bold)"
+export WHITEBOLD="${WHITE}$(tput bold)"
+export NCNORMAL="${NC}$(tput sgr0)"
 
 if echo "$agents" | grep -q "$address"; then
-  echo "Correct configuration setup!"
+  echo -e "${GREENBOLD}Correct configuration setup! (agent ${address})${NC}"
 else
-  echo "Invalid configuration setup!"
+  echo -e "${REDBOLD}Invalid configuration setup! (active agent ${agents}. expected ${address})   ${NC}"
 fi
