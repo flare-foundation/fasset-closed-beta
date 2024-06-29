6a550ea38dec575ced4566712e2bbcc62fda416a
david.pangerl <david.pangerl@flare.network>
2024-06-29 17:20:08 +0200

    new test controller

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-address=0xb11F5a3162ec5b4d55034E27b1B277A8D38716C9
+address=0x6Ca072Ec384A224F88B5d30aF8B307bd417EC0ef
 agents=$(./user-bot.sh agents --all -f FSimCoinX | grep -Eo ^[0-9a-zA-Z]{42})
 
 export RED='\033[0;31m'
