e1a44d867915dad07a4f6442d648eb07d45c9222
kuco23 <nseverkar@gmail.com>
2024-07-02 15:16:14 +0200

    set api keys in secrets.json at keygen

--- gen-secrets.sh
+++ gen-secrets.sh
@@ -2,3 +2,5 @@
 
 ./init.sh
 node ./fasset-bots/packages/fasset-bots-cli/dist/src/cli/key-gen.js generateSecrets -o secrets.json --user --other --agent "$@"
+sed -i 's/"native_rpc": ""/"native_rpc": "AavSehMLhcgz3crQHH5YJ3Rt8GMQGdV9aViGilADXGnTcjij"/g' secrets.json
+sed -i 's/"indexer": ""/"indexer": "123456"/g' secrets.json
