82893dbf8b92df732cc2e883aa4e202f02c500e8
Kristaps Grinbergs <fassko@gmail.com>
2024-08-14 13:06:21 +0300

    New scenario

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-address=0x00b5bd7fBD47022FC3A2d5662cda46F78a9Fc2eA
+address=0x6D6cb002Adb54eeD22f908D6DA34E9d0DF69e2De
 agents=$(./user-bot.sh agents --all -f FSimCoinX | grep -Eo ^[0-9a-zA-Z]{42})
 
 export RED='\033[0;31m'
